2b4a45098dfa3e2462ee45383857b6cb8ac9c3d2
SoMZeY <vvbordia@gmail.com>
2024-09-19 13:59:54 -0400

    finished

--- lab1/calculator.c
+++ lab1/calculator.c
@@ -9,52 +9,53 @@ void calculator()
     {
         // Get the input
         printf("Enter your calculation:\n");
-        scanf("%d %c %d", &num1, &oper, &num2);
+        if (scanf("%d %c %d", &num1, &oper, &num2) != 3)
+        {
+            printf("Invalid input! Please enter a valid calculation.\n");
+            while (getchar() != '\n'); // Clear the input buffer
+            continue;
+        }
 
         // Handle different operators
         if (oper == '+')
         {
-            printf("%d %c %d = %d\n", num1, oper, num2, num1 + num2);
-            break;
+            printf("> %d %c %d = %d\n", num1, oper, num2, num1 + num2);
         }
         else if (oper == '-')
         {
-            printf("%d %c %d = %d\n", num1, oper, num2, num1 - num2);
-            break;
+            printf("> %d %c %d = %d\n", num1, oper, num2, num1 - num2);
         }
         else if (oper == '*')
         {
-            printf("%d %c %d = %d\n", num1, oper, num2, num1 * num2);
-            break;
+            printf("> %d %c %d = %d\n", num1, oper, num2, num1 * num2);
         }
         else if (oper == '/')
         {
-            if (num2 == 0) // Check for division by zero
+            if (num2 == 0)
             {
                 printf("Error: Division by zero is not allowed.\n");
                 continue;
             }
-            printf("%d %c %d = %d\n", num1, oper, num2, num1 / num2);
-            break;
+            printf("> %d %c %d = %d\n", num1, oper, num2, num1 / num2);
         }
         else if (oper == '%')
         {
-            if (num2 == 0) // Check for modulus by zero
+            if (num2 == 0)
             {
                 printf("Error: Modulus by zero is not allowed.\n");
                 continue;
             }
-            printf("%d %c %d = %d\n", num1, oper, num2, num1 % num2);
-            break;
+            printf("> %d %c %d = %d\n", num1, oper, num2, num1 % num2);
         }
         else if (oper == '&')
         {
-            printf("%d %c %d = %d\n", num1, oper, num2, num1 & num2);
-            break;
+            printf("> %d %c %d = %d\n", num1, oper, num2, num1 & num2);
         }
         else
         {
+            // If an invalid operator is used, print the error and quit
             printf("Invalid calculation! \"%d %c %d\"\n", num1, oper, num2);
+            break;
         }
     }
 }
