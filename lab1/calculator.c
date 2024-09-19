15369460488f8e9b4330f5744ada58eb9e79f2df
SoMZeY <vvbordia@gmail.com>
2024-09-19 13:37:29 -0400

    maybe finished now?

--- lab1/calculator.c
+++ lab1/calculator.c
@@ -1,55 +1,66 @@
-#include "calculator.h"
+#include <stdio.h>
 
 void calculator()
 {
-	int num1, num2;
-	char oper;
-	
-	while(1)
-	{
-		// Get the input
-		printf("Enter your calculation: \n");
-		scanf("%d %c %d", &num1, &oper, &num2);
-	
-		// I'm too lazy to do this elegantly, so enjoy this crappy code
-		if (oper == '+')
-		{
-			printf("%d %c %d = %d\n", num1, oper, num2, num1 + num2);
-			break;
-		}
-		else if (oper == '-')
-		{
+    int num1, num2;
+    char oper;
 
-			printf("%d %c %d = %d\n", num1, oper, num2, num1 - num2);
-			break;
-		}
-		else if (oper == '*')
-		{
+    while (1)
+    {
+        // Get the input
+        printf("Enter your calculation (e.g., 2 + 2): \n");
+        scanf("%d %c %d", &num1, &oper, &num2);
 
-			printf("%d %c %d = %d\n", num1, oper, num2, num1 * num2);
-			break;
-		}
-		else if (oper == '/')
-		{
+        // Handle different operators
+        if (oper == '+')
+        {
+            printf("%d %c %d = %d\n", num1, oper, num2, num1 + num2);
+            break;
+        }
+        else if (oper == '-')
+        {
+            printf("%d %c %d = %d\n", num1, oper, num2, num1 - num2);
+            break;
+        }
+        else if (oper == '*')
+        {
+            printf("%d %c %d = %d\n", num1, oper, num2, num1 * num2);
+            break;
+        }
+        else if (oper == '/')
+        {
+            if (num2 == 0) // Check for division by zero
+            {
+                printf("Error: Division by zero is not allowed.\n");
+                continue;
+            }
+            printf("%d %c %d = %d\n", num1, oper, num2, num1 / num2);
+            break;
+        }
+        else if (oper == '%')
+        {
+            if (num2 == 0) // Check for modulus by zero
+            {
+                printf("Error: Modulus by zero is not allowed.\n");
+                continue;
+            }
+            printf("%d %c %d = %d\n", num1, oper, num2, num1 % num2);
+            break;
+        }
+        else if (oper == '&')
+        {
+            printf("%d %c %d = %d\n", num1, oper, num2, num1 & num2);
+            break;
+        }
+        else
+        {
+            printf("Invalid calculation! \"%d %c %d\"\n", num1, oper, num2);
+        }
+    }
+}
 
-			printf("%d %c %d = %d\n", num1, oper, num2, num1 / num2);
-			break;
-		}
-		else if (oper == '%')
-		{
-
-			printf("%d %c %d = %d\n", num1, oper, num2, num1 % num2);
-			break;
-		}
-		else if (oper == '&')
-		{
-
-			printf("%d %c %d = %d\n", num1, oper, num2, num1 & num2);
-			break;
-		}	
-		else 
-		{
-			printf("Invalid calculation! \"%d %c %d\"\n", num1, oper, num2);
-		}
-	}
+int main()
+{
+    calculator();
+    return 0;
 }
